88c5dceae67ee103e4a890967ffea316a92c72f4
mongsik98 <olivetree421@gmail.com>
2022-06-01 19:06:17 +0900

    listicon

--- frontend/src/components/views/HomePage/HomePage.jsx
+++ frontend/src/components/views/HomePage/HomePage.jsx
@@ -8,6 +8,8 @@ import { ListView } from "../../utils/ListVIew";
 import { IconView } from "../../utils/IconView/IconView";
 import { useDispatch } from "react-redux";
 import { folderActions } from "../../../_actions";
+import { faFolder } from '@fortawesome/free-solid-svg-icons';
+import { faFileLines } from '@fortawesome/free-regular-svg-icons';
 
 function HomePage() {
     const dispatch = useDispatch();
@@ -16,6 +18,30 @@ function HomePage() {
     const [isViewModal, setisViewModal] = useState(false);
 
     const u_id = localStorage.getItem('user');
+
+    const onFolderHandler = (item)=> {
+        if (item[2] === true) {
+            // 현재 폴더 변경
+            
+            // 페이지 리다이렉트
+
+        } else {
+            // 파일 미리보기 
+        }
+    }
+
+
+    // 폴더 정보 요청 응답 예시
+    const response = {
+        folderId: '123',
+        folderName: 'user123',
+        made_by: 'user123',
+        max_volume: 100,
+        pres_volume: 20,
+        kinds: 0,
+        items: [['folder1','캡스톤디자인', true], ['folder2','클라우드컴퓨팅',true]]
+    }
+    const itemList = response.items
 
     // 페이지 첫 렌더링 시 폴더 정보 요청
     /*useEffect(() => {
@@ -88,8 +114,45 @@ function HomePage() {
             <div className="Views">
                 {
                     view === 'icon'
-                    ? (<ListView/>)
-                    : (<IconView/>)
+                    ? (<div className="ListView">
+                    <div className="ListColDescription">
+                        <div>강의실 이름</div>
+                    </div>
+                    {
+                        itemList.map(function(item){
+                            return (
+                                <div className="ListItem" onDoubleClick={()=>onFolderHandler(item)} key={item[0]} >
+                                    <div className="ListIconBox">
+                                    {
+                                        item[2] === true 
+                                        ? (<FontAwesomeIcon icon={faFolder} className="ListFolderIcon"/>)
+                                        : (<FontAwesomeIcon icon={faFileLines} className="ListFileIcon"/>)
+                                    }
+                                    </div>
+                                    <div className="ListNameBox">
+                                        <p>{item[1]}</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>)
+                    : (<ul className="IconList">
+                    {itemList.map(item => (
+                        <li onDoubleClick={()=>onFolderHandler(item)} key={item[0]}> 
+                            <div className="Iconbox">
+                                    {
+                                        item[2] === true 
+                                        ? (<FontAwesomeIcon icon={faFolder} className="FolderIcon"/>)
+                                        : (<FontAwesomeIcon icon={faFileLines} className="FileIcon"/>)
+                                    }
+                            </div>
+                            <div className="ListNameBox">
+                                <p>{item[1]}</p>
+                            </div>                     
+                        </li>
+                    ))} 
+                </ul>)
                 }
             </div>
             {/* 강의실 생성 모달 */}
