986d926f7bb12faedc19eb72078518ffff41f9e7
mongsik98 <olivetree421@gmail.com>
2022-06-02 01:21:09 +0900

    requst

--- frontend/src/components/views/LandingPage/LandingPage.jsx
+++ frontend/src/components/views/LandingPage/LandingPage.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import rootReducer from '../../../_reducers';
 
 
 function LandingPage() {
 
+    const now = useSelector(rootReducer=>rootReducer.folder)
 
+    useEffect(()=>{
+        console.log(now)
+    })
     return (
         <div>
             시작 페이지
